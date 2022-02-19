5a11ab0cee0780bc422b829c3b2741ce96e120d1
Iago Virgílio <iagovirgilio@gmail.com>
2022-02-18 21:54:37 -0300

    Img user in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,28 @@ function Header() {
                 <img src="/images/home-icon.svg" />
                 <span>HOME</span>
             </a>
+            <a>
+                <img src="/images/search-icon.svg" />
+                <span>SEARCH</span>
+            </a>
+            <a>
+                <img src="/images/watchlist-icon.svg" />
+                <span>WATCHLIST</span>
+            </a>
+            <a>
+                <img src="/images/original-icon.svg" />
+                <span>ORIGINALS</span>
+            </a>
+            <a>
+                <img src="/images/movie-icon.svg" />
+                <span>MOVIES</span>
+            </a>
+            <a>
+                <img src="/images/series-icon.svg" />
+                <span>SERIES</span>
+            </a>
         </NavMenu>
+        <UserImage src="https://avatars.githubusercontent.com/u/33989073?v=4"/>
     </Nav>
   )
 }
@@ -30,5 +51,33 @@ const Logo = styled.img`
 `;
 
 const NavMenu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 25px
+    align-items: center;
+    flex-wrap: wrap;
 
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+
+        img {
+            height: 20px;
+        }
+
+        span {
+            font-size: 13px;
+            letter-spacing: 1.42px;
+        }
+    }
+`;
+
+
+const UserImage = styled.img`
+    width: 48px;
+    height: 48px;
+    border-radius: 50%;
+    cursor: pointer;
 `;
